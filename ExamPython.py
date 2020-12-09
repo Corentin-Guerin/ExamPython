c326286ef28a8f55bbc41eb29e255d359cc89300
Corentin-Guerin <cocog53200@gmail.com>
2020-12-09 15:19:08 +0100

    verife6lettre

--- ExamPython.py
+++ ExamPython.py
@@ -1,18 +1,38 @@
 from colorama
 import init init() 
-from colorama import Fore, Back, Style 
+from colorama import Fore, Back, Style
+
+
+
 import random
-
-
 
 def motus():
 
-    mot=["mot0","mot1","mot2","mot3","mot4","mot5","mot6","mot7","mot8","mot9"]
+    mot=["mmmot0","mmmot1","mmmot2","mmmot3","mmmot4","mmmot5","mmmot6","mmmot7","mmmot8","mmmot9"]
     valmotale = random.randint(0,9)
     motale = mot[valmotale]
+    print(motale)      #Temporaire info mot choisie
     
-    print(motale)
+    
+    propmot=propositionmot(motale)
 
-motus():    
+
+def propositionmot(mot):
+
+    propmot=input("Entre un mot de 6 lettre :")
+    while (len(propmot) != 6):
+        print("Le mot etre ne contien pas 6 lettre : Recommencer !")#vérifie que le mot choisie a 6 lettre 
+        propmot=input("Entre un mot de 6 lettre :")
+    print(propmot)
+    return(propmot)
+
+
+
+
+motus()  
+
+    
+   
+
 
 #propmot=int(input("Entre un mot de 6 lettre :"))
